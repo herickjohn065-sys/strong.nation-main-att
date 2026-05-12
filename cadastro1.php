e86cb9d1ea80f3f05fd3c82c9147de125f6004bf
herickjohn065-sys <herickjohn065@gmail.com>
2026-05-11 21:34:13 -0300

    alterado2

--- cadastro1.php
+++ cadastro1.php
@@ -1,26 +1,22 @@
 <?php
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
     $nome = $_POST['nome'];
     $cpf = $_POST['cpf'];
     $telefone = $_POST['telefone'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-    $confirmar = $_POST['confirmar_senha'];
-    $pass=$senha;
-$md5=md5($pass);
-$hash = password_hash($pass, PASSWORD_DEFAULT);
-echo "<hr>";
-echo "Senha: $senha";
-echo "<br>Pass: $pass";
-echo "<br>MD5: $md5";
-echo "<br>hash: $hash";
-    
-    if ($senha === $confirmar) {
-    
-        header('Location: clienteHome.html');
-        exit();
-    } else {
-        echo "Senhas não conferem!";
+
+    $senhacriptografada = md5($senha);
+
+    header("Location: clienteHome.html");
+    exit();
+
     }
-}
+    
+    else {
+        echo "coloque uma senha";
+        }
+    
 ?>
--- cadastroacademia.php
+++ cadastroacademia.php
@@ -1,25 +1,22 @@
 <?php
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nome = $_POST['nome'];
-    $cpf = $_POST['cpf'];
-    $telefone = $_POST['telefone'];
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-    $confirmar = $_POST['confirmar_senha'];
-    $pass="StrongNation";
-$md5=md5($pass);
-$hash = password_hash($pass, PASSWORD_DEFAULT);
-echo "<hr>";
-echo "<br>Pass: $pass";
-echo "<br>MD5: $md5";
-echo "<br>hash: $hash";
-    
-    if ($senha === $confirmar) {
-    
-        header('Location: academiaHome.html');
-        exit();
-    } else {
-        echo "Senhas não conferem!";
+
+    $nome = $_POST['Nome'];
+    $cpf = $_POST['CNPJ'];
+    $telefone = $_POST['Telefone'];
+    $email = $_POST['Email'];
+    $senha = $_POST['Senha'];
+
+    $senhacriptografada = md5($senha);
+
+    header("Location: academiaHome.html");
+    exit();
+
     }
-}
+    
+    else {
+        echo "coloque uma senha";
+        }
+    
 ?>
