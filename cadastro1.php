f31f587b8a6e32413276fce8fed4eafc35fc8eef
SergioRCJunior <sergiorobertocarnelossojr1207@gmail.com>
2026-05-16 18:45:45 -0300

    Att Banco de Dados

--- cadastro1.php
+++ cadastro1.php
@@ -1,22 +1,31 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+include("conexao.php");
 
-    $nome = $_POST['nome'];
-    $cpf = $_POST['cpf'];
-    $telefone = $_POST['telefone'];
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+$nome = $_POST['nome'];
+$cpf = $_POST['cpf'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
-    $senhacriptografada = md5($senha);
+$senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO clientes
+(nome, cpf, telefone, email, senha)
+VALUES
+('$nome', '$cpf', '$telefone', '$email', '$senhaCriptografada')";
+
+if ($conexao->query($sql) === TRUE) {
 
     header("Location: clienteHome.html");
     exit();
 
-    }
-    
-    else {
-        echo "coloque uma senha";
-        }
-    
+} else {
+
+    echo "Erro: " . $conexao->error;
+
+}
+
+$conexao->close();
+
 ?>
--- cadastroacademia.php
+++ cadastroacademia.php
@@ -1,22 +1,31 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+include("conexao.php");
 
-    $nome = $_POST['Nome'];
-    $cpf = $_POST['CNPJ'];
-    $telefone = $_POST['Telefone'];
-    $email = $_POST['Email'];
-    $senha = $_POST['Senha'];
+$nome = $_POST['nome'];
+$cnpj = $_POST['cnpj'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
-    $senhacriptografada = md5($senha);
+$senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO academias
+(nome, cnpj, telefone, email, senha)
+VALUES
+('$nome', '$cnpj', '$telefone', '$email', '$senhaCriptografada')";
+
+if ($conexao->query($sql) === TRUE) {
 
     header("Location: academiaHome.html");
     exit();
 
-    }
-    
-    else {
-        echo "coloque uma senha";
-        }
-    
+} else {
+
+    echo "Erro: " . $conexao->error;
+
+}
+
+$conexao->close();
+
 ?>
